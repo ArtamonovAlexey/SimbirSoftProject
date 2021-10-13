dd493759f2f676c58c7b9a08b51ac4304ab881f1
Алексей Артамонов <artamonov@MacBook-Air-Aleksej.local>
2021-10-14 00:08:42 +0300

    delete comments in class CounterWords

--- src/main/java/ru/artamonov/ParserHtml.java
+++ src/main/java/ru/artamonov/ParserHtml.java
@@ -4,10 +4,6 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 
 import java.io.IOException;
-//import java.util.Arrays;
-//import java.util.List;
-//import java.util.TreeMap;
-//import java.util.stream.Collectors;
 
 public class ParserHtml {
     public static String getText(String url) throws IOException {
